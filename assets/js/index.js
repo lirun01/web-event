50e8ffc09ecb13428e7c36dc3a63e6b7012155dd
李润 <9653409+li-run1@user.noreply.gitee.com>
2022-07-16 23:08:42 +0800

    完成了主页的开发

---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+$(function () {
+    getUserinfo()
+    $('#btnlogin').on('click', function () {
+        // console.log('ok');
+        layer.confirm('确定退出吗？', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+
+function getUserinfo() {
+    $.ajax({
+        method: 'GET',
+        url: 'http://www.liulongbin.top:3007/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('请求失败')
+            }
+            renderAvatar(res.data)
+        },
+        complete:function(res){
+            // console.log(res);
+            if(res.responseJSON.status ===1&&res.responseJSON.message){
+                localStorage.removeItem('token')
+                location.href = '/login.html'
+            }
+        }
+    })
+}
+
+//渲染用户名称
+function renderAvatar(user) {
+    let name = user.nickname || user.username
+    $('#welcom').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').atter(src, user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        let first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
